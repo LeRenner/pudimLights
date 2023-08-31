419ac670bc60968f9871331b1777374407fb8117
J. Renner <j@lerenner.dev>
2023-08-31 20:00:37 -0300

    Read mqtt broker hostname and port from file
    Signed-off-by: J. Renner <j@lerenner.dev>

--- backend/main.js
+++ backend/main.js
@@ -4,30 +4,39 @@ const app = express()
 const port = 3000
 
 var mqtt = require('mqtt');
-var client = mqtt.connect("mqtt://mosquitto");
+
+// read mqtt broker address from /config/hostname
+var hostname = require('fs').readFileSync('/config/hostname', 'utf-8').trim();
+
+// read mqtt broker port from /config/port
+var mqttPort = require('fs').readFileSync('/config/port', 'utf-8').trim();
+
+//connect 
+var client = mqtt.connect('mqtt://' + hostname + ':' + mqttPort);
+
 console.log(client)
 
 app.get('/1/on', (req, res) => {
   console.log("/1/on")
-  client.publish("PLS/abajour", "1");
+  client.publish("PLS/1", "1");
   res.send('Sucess!');
 })
 
 app.get('/1/off', (req, res) => {
   console.log("/1/off")
-  client.publish("PLS/abajour", "0");
+  client.publish("PLS/1", "0");
   res.send('Sucess!');
 })
 
 app.get('/2/on', (req, res) => {
   console.log("/2/on")
-  client.publish("PLS/mesa", "1");
+  client.publish("PLS/2", "1");
   res.send('Sucess!');
 })
 
 app.get('/2/off', (req, res) => {
   console.log("/2/off")
-  client.publish("PLS/mesa", "0");
+  client.publish("PLS/2", "0");
   res.send('Sucess!');
 })
 
